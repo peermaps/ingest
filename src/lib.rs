0cb80108f5435c51c6296c099ec10272a30b46ec
okdistribute <633012+okdistribute@users.noreply.github.com>
2021-01-12 19:32:12 -1000

    cleanup: reorder lib

--- src/lib.rs
+++ src/lib.rs
@@ -19,85 +19,12 @@ use tags;
 use vadeen_osm::osm_io::error::Error;
 use vadeen_osm::{geo::Coordinate, Node, Relation, Way};
 
-#[test]
-fn read_write_fixture() {
-    use crate::Writer;
-    // Create a builder.
-    let mut builder = OsmBuilder::default();
-
-    // Add a polygon to the map.
-    builder.add_polygon(
-        vec![
-            vec![
-                // Outer polygon
-                (66.29, -3.177),
-                (66.29, -0.9422),
-                (64.43, -0.9422),
-                (64.43, -3.177),
-                (66.29, -3.177),
-            ],
-            vec![
-                // One inner polygon
-                (66.0, -2.25),
-                (65.7, -2.5),
-                (65.7, -2.0),
-                (66.0, -2.25),
-            ],
-            // Add more inner polygons here.
-        ],
-        vec![("natural", "water")],
-    );
-
-    // Add polyline to the map.
-    builder.add_polyline(vec![(66.29, 1.2), (64.43, 1.2)], vec![("power", "line")]);
-
-    // Add point
-    builder.add_point((66.19, 1.3), vec![("power", "tower")]);
-
-    // Build into Osm structure.
-    let osm = builder.build();
-
-    let output = "testoutput_rw";
-    let mut writer = Writer::new(output);
-
-    let node = osm.nodes[0].clone();
-    let rel = osm.relations[0].clone();
-    let way = osm.ways[0].clone();
-    println!("{}", way.id);
-
-    writer.add_node(osm.nodes[0].clone());
-    writer.add_way(osm.ways[0].clone());
-    writer.add_relation(osm.relations[0].clone());
-
-    let reader = Reader::new(output);
-    let read_node = reader.read_node(node.id);
-    let read_way = reader.read_way(way.id);
-    let read_rel = reader.read_relation(rel.id);
-
-    assert_eq!(read_node.id, node.id);
-    assert_eq!(read_node.coordinate, node.coordinate);
-    assert_eq!(read_node.meta, node.meta);
-
-    assert_eq!(read_way.id, way.id);
-    assert_eq!(read_way.refs, way.refs);
-    assert_eq!(read_way.meta, way.meta);
-
-    assert_eq!(read_rel.id, rel.id);
-    assert_eq!(read_rel.members, rel.members);
-    assert_eq!(read_rel.meta, rel.meta);
-
-    let db_path = "test_db";
-
-    write_to_db(output, "test_db");
-    fs::remove_dir_all(output);
-}
-
 type P = Mix2<f32, f32>;
 type V = u32;
 type E = Box<dyn std::error::Error + Sync + Send>;
 
 #[async_std]
-pub fn write_to_db(pbf: &str, db: &str) -> Result<(), E> {
+pub fn write_to_db(output: &str, db: &str) -> Result<(), E> {
     let reader = Reader::new(output);
     let mut db: DB<_, P, V> = DB::open_from_path(&PathBuf::from(db)).await?;
 
@@ -192,4 +119,77 @@ pub fn denormalize(pbf: &str, output: &str) -> std::result::Result<bool, Error> 
     println!("Wrote {} elements", total);
 
     return Ok(true);
+}
+
+#[test]
+fn read_write_fixture() {
+    use crate::Writer;
+    // Create a builder.
+    let mut builder = OsmBuilder::default();
+
+    // Add a polygon to the map.
+    builder.add_polygon(
+        vec![
+            vec![
+                // Outer polygon
+                (66.29, -3.177),
+                (66.29, -0.9422),
+                (64.43, -0.9422),
+                (64.43, -3.177),
+                (66.29, -3.177),
+            ],
+            vec![
+                // One inner polygon
+                (66.0, -2.25),
+                (65.7, -2.5),
+                (65.7, -2.0),
+                (66.0, -2.25),
+            ],
+            // Add more inner polygons here.
+        ],
+        vec![("natural", "water")],
+    );
+
+    // Add polyline to the map.
+    builder.add_polyline(vec![(66.29, 1.2), (64.43, 1.2)], vec![("power", "line")]);
+
+    // Add point
+    builder.add_point((66.19, 1.3), vec![("power", "tower")]);
+
+    // Build into Osm structure.
+    let osm = builder.build();
+
+    let output = "testoutput_rw";
+    let mut writer = Writer::new(output);
+
+    let node = osm.nodes[0].clone();
+    let rel = osm.relations[0].clone();
+    let way = osm.ways[0].clone();
+    println!("{}", way.id);
+
+    writer.add_node(osm.nodes[0].clone());
+    writer.add_way(osm.ways[0].clone());
+    writer.add_relation(osm.relations[0].clone());
+
+    let reader = Reader::new(output);
+    let read_node = reader.read_node(node.id);
+    let read_way = reader.read_way(way.id);
+    let read_rel = reader.read_relation(rel.id);
+
+    assert_eq!(read_node.id, node.id);
+    assert_eq!(read_node.coordinate, node.coordinate);
+    assert_eq!(read_node.meta, node.meta);
+
+    assert_eq!(read_way.id, way.id);
+    assert_eq!(read_way.refs, way.refs);
+    assert_eq!(read_way.meta, way.meta);
+
+    assert_eq!(read_rel.id, rel.id);
+    assert_eq!(read_rel.members, rel.members);
+    assert_eq!(read_rel.meta, rel.meta);
+
+    let db_path = "test_db";
+
+    write_to_db(output, "test_db");
+    fs::remove_dir_all(output);
 }
