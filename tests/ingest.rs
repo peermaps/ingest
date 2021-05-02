22acd9bbcab61a19317276e88781c08a1d408fce
substack <substack@bits.coop>
2021-05-02 09:26:08 -1000

    another failing test

--- tests/ingest.rs
+++ tests/ingest.rs
@@ -4,6 +4,7 @@ use async_std::{prelude::*,fs::File};
 use tempfile::Builder as Tmpfile;
 use eyros::{Coord as C};
 use georender_pack::{Feature,Point,Area};
+use pretty_assertions::assert_eq;
 
 type Error = Box<dyn std::error::Error+Send+Sync>;
 
@@ -21,6 +22,8 @@ async fn ingest() -> Result<(),Error> {
   o5c_file0.push("tests/data/changeset0.o5c");
   let mut o5c_file1 = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
   o5c_file1.push("tests/data/changeset1.o5c");
+  let mut o5c_file2 = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+  o5c_file2.push("tests/data/changeset2.o5c");
 
   let mut ingest = Ingest::new(
     LStore::new(open(std::path::Path::new(&ldb_dir))?),
@@ -212,6 +215,82 @@ async fn ingest() -> Result<(),Error> {
           id: 2000*3+0,
           feature_type: get_type("highway.bus_stop"),
           point: (13.03,37.04),
+          labels: vec![0],
+        })),
+      ]
+    ];
+  }
+
+  ingest.changeset(Box::new(File::open(&o5c_file2).await?)).await?;
+  {
+    let mut estore = ingest.estore.lock().await;
+    let mut stream = estore.db.query(&((3.0,-15.0),(15.0,45.0))).await?;
+    let mut results = vec![];
+    while let Some(result) = stream.next().await {
+      let (pt,v) = result?;
+      results.push((pt,georender_pack::decode(&v.data)?));
+    }
+    results.sort_by(|a,b| {
+      let a_id = get_id(&a.1);
+      let b_id = get_id(&b.1);
+      fn get_id(x: &georender_pack::Feature) -> u64 {
+        match x {
+          georender_pack::Feature::Point(x) => x.id,
+          georender_pack::Feature::Line(x) => x.id,
+          georender_pack::Feature::Area(x) => x.id,
+        }
+      }
+      match (a_id < b_id, a_id > b_id) {
+        (true,_) => std::cmp::Ordering::Less,
+        (_,true) => std::cmp::Ordering::Greater,
+        (false,false) => std::cmp::Ordering::Equal,
+      }
+    });
+    let w_ex_positions = vec![
+      5.004, -10.006, 5.005, -10.006, 5.005, -10.007, 5.003, -10.008,
+    ];
+    let w_ex_cells = earcutr::earcut(&w_ex_positions.iter()
+      .map(|p| *p as f64).collect(), &vec![], 2);
+    let r_ex_positions = vec![
+      4.999,  -9.999, 5.000, -10.010, 5.010, -10.010, 5.001, -10.001,
+      5.005, -10.003, 5.006, -10.004, 5.007, -10.003,
+      5.004, -10.006, 5.005, -10.006, 5.005, -10.007, 5.003, -10.008,
+    ];
+    let r_ex_cells = earcutr::earcut(&r_ex_positions.iter()
+      .map(|p| *p as f64).collect(), &vec![4,7], 2);
+    assert_eq![
+      results,
+      vec![
+        ((C::Interval(5.003,5.005),C::Interval(-10.008,-10.006)), Feature::Area(Area {
+          id: 602*3+1,
+          feature_type: get_type("place.island"),
+          positions: w_ex_positions,
+          cells: w_ex_cells,
+          labels: vec![0],
+        })),
+        ((C::Interval(4.999,5.010),C::Interval(-10.010,-9.999)), Feature::Area(Area {
+          id: 700*3+2,
+          feature_type: get_type("natural.water"),
+          positions: r_ex_positions,
+          cells: r_ex_cells,
+          labels: "\x0a=Cool Lake\x00".as_bytes().to_vec(),
+        })),
+        ((C::Scalar(13.02),C::Scalar(37.00)), Feature::Point(Point {
+          id: 1312*3+0,
+          feature_type: get_type("amenity.cafe"),
+          point: (13.02,37.00),
+          labels: vec![0],
+        })),
+        ((C::Scalar(13.03),C::Scalar(37.04)), Feature::Point(Point {
+          id: 2000*3+0,
+          feature_type: get_type("highway.bus_stop"),
+          point: (13.03,37.04),
+          labels: vec![0],
+        })),
+        ((C::Scalar(5.003),C::Scalar(-10.008)), Feature::Point(Point {
+          id: 9104*3+0,
+          feature_type: get_type("amenity.boat_rental"),
+          point: (5.003,-10.008),
           labels: vec![0],
         })),
       ]
