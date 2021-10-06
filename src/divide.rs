73bd7dedb8067e6d129a74a83d4b280664110d2e
substack <substack@bits.coop>
2021-10-06 00:45:09 -1000

    recursive optimizer gets down to 1.5M in the test, the best yet by far (previous best 2.4M)

--- src/divide.rs
+++ src/divide.rs
@@ -1,10 +1,74 @@
+use std::collections::VecDeque;
 use crate::value::V;
 use eyros::tree::InsertValue;
 
 type P = (eyros::Coord<f32>,eyros::Coord<f32>);
 type B = ((f32,f32),(f32,f32));
-type I<'a> = (B,&'a [(P,InsertValue<'a,P,V>)]);
+type I<'a> = (B,Vec<(P,InsertValue<'a,P,V>)>);
 
 pub fn divide<'a>(n: usize, bucket: I<'a>) -> Vec<I<'a>> {
-  unimplemented![]
+  if bucket.1.len() <= n {
+    return vec![bucket];
+  }
+  let (nx,ny) = (4,4);
+  let mut res = vec![];
+  let mut queue = VecDeque::new();
+  queue.push_back(bucket);
+  while let Some((q_bbox,q_inserts)) = queue.pop_front() {
+    let mut boxes: Vec<I<'a>> = Vec::with_capacity(nx*ny);
+    for iy in 0..ny {
+      for ix in 0..nx {
+        let bbox = (
+          (
+            (ix as f32 / nx as f32) * ((q_bbox.1).0 - (q_bbox.0).0) + (q_bbox.0).0,
+            (iy as f32 / ny as f32) * ((q_bbox.1).1 - (q_bbox.0).1) + (q_bbox.0).1,
+          ),
+          (
+            ((ix as f32 + 1.0) / nx as f32) * ((q_bbox.1).0 - (q_bbox.0).0) + (q_bbox.0).0,
+            ((iy as f32 + 1.0) / ny as f32) * ((q_bbox.1).1 - (q_bbox.0).1) + (q_bbox.0).1,
+          ),
+        );
+        boxes.push((bbox,vec![]));
+      }
+    }
+    for (p,insert) in q_inserts {
+      // find the bbox with the largest overlap
+      let mut best = (0.0, 0);
+      for (j,(bbox,_)) in boxes.iter().enumerate() {
+        let area = overlap_area(&bbox, &p);
+        if area > best.0 {
+          best = (area, j);
+        }
+      }
+      boxes[best.1].1.push((p,insert));
+    }
+    for (bbox,inserts) in boxes {
+      if !inserts.is_empty() && inserts.len() <= n {
+        res.push((bbox,inserts));
+      } else if !inserts.is_empty() {
+        queue.push_back((bbox,inserts));
+      }
+    }
+  }
+  res
+}
+
+fn overlap_area(bbox: &B, p: &P) -> f32 {
+  match p {
+    (eyros::Coord::Scalar(x),eyros::Coord::Scalar(y)) => {
+      if &(bbox.0).0 <= x && x <= &(bbox.1).0 && &(bbox.0).1 <= y && y <= &(bbox.1).1 {
+        1.0
+      } else {
+        0.0
+      }
+    },
+    (eyros::Coord::Interval(x0,x1),eyros::Coord::Interval(y0,y1)) => {
+      if &(bbox.0).0 > x1 || &(bbox.1).0 < x0 { return 0.0; }
+      if &(bbox.0).1 > y1 || &(bbox.1).1 < y0 { return 0.0; }
+      let x = x1.min((bbox.1).0) - x0.max((bbox.0).0);
+      let y = y1.min((bbox.1).1) - y0.max((bbox.0).1);
+      x * y
+    },
+    _ => 0.0
+  }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -505,7 +505,7 @@ impl Ingest {
           let q_inserts = values.iter()
             .map(|(p,v)| (p.clone(),eyros::tree::InsertValue::Value(v)))
             .collect::<Vec<_>>();
-          for (bbox,inserts) in divide(100_000,(q_bbox,&q_inserts)) {
+          for (bbox,inserts) in divide(100_000,(q_bbox,q_inserts)) {
             let (o_tr, create_trees) = T::build(
               out_db.fields.clone(),
               &inserts,
@@ -519,8 +519,8 @@ impl Ingest {
             }
             println![
               "({:.4},{:.4},{:.4},{:.4}) {}",
-              (q_bbox.0).0, (q_bbox.0).1, (q_bbox.1).0, (q_bbox.1).1,
-              values.len()
+              (bbox.0).0, (bbox.0).1, (bbox.1).0, (bbox.1).1,
+              inserts.len()
             ];
             tree_refs.push((P::from_bounds(&q_bbox), eyros::tree::InsertValue::Ref(tr)));
           }
@@ -536,8 +536,10 @@ impl Ingest {
       }
     }
     {
+      let mut fields = (*out_db.fields).clone();
+      fields.rebuild_depth = 0;
       let (tr, create_trees) = T::build(
-        out_db.fields.clone(),
+        Arc::new(fields),
         &tree_refs,
         &mut out_db.meta.next_tree,
         false
@@ -549,7 +551,7 @@ impl Ingest {
       out_db.meta.roots.push(tr);
     }
     out_db.sync().await?;
-    out_db.optimize(4).await?;
+    //out_db.optimize(4).await?;
     self.progress.write().await.add("optimize", 0);
     self.progress.write().await.end("optimize");
     Ok(())
