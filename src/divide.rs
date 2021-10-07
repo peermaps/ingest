420c57fd640d220a2e55361d994c94315c3a693c
substack <substack@bits.coop>
2021-10-06 19:18:05 -1000

    check to stop recursion if there are too many large spanning records

--- src/divide.rs
+++ src/divide.rs
@@ -16,21 +16,26 @@ pub fn divide<'a>(n: usize, bucket: I<'a>) -> Vec<I<'a>> {
   queue.push_back(bucket);
   while let Some((q_bbox,q_inserts)) = queue.pop_front() {
     let mut boxes: Vec<I<'a>> = Vec::with_capacity(nx*ny);
+    let q_span = (
+      (q_bbox.1).0 - (q_bbox.0).0,
+      (q_bbox.1).1 - (q_bbox.0).1,
+    );
     for iy in 0..ny {
       for ix in 0..nx {
         let bbox = (
           (
-            (ix as f32 / nx as f32) * ((q_bbox.1).0 - (q_bbox.0).0) + (q_bbox.0).0,
-            (iy as f32 / ny as f32) * ((q_bbox.1).1 - (q_bbox.0).1) + (q_bbox.0).1,
+            (ix as f32 / nx as f32) * q_span.0 + (q_bbox.0).0,
+            (iy as f32 / ny as f32) * q_span.1 + (q_bbox.0).1,
           ),
           (
-            ((ix as f32 + 1.0) / nx as f32) * ((q_bbox.1).0 - (q_bbox.0).0) + (q_bbox.0).0,
-            ((iy as f32 + 1.0) / ny as f32) * ((q_bbox.1).1 - (q_bbox.0).1) + (q_bbox.0).1,
+            ((ix as f32 + 1.0) / nx as f32) * q_span.0 + (q_bbox.0).0,
+            ((iy as f32 + 1.0) / ny as f32) * q_span.1 + (q_bbox.0).1,
           ),
         );
         boxes.push((bbox,vec![]));
       }
     }
+    let q_inserts_len = q_inserts.len();
     for (p,insert) in q_inserts {
       // find the bbox with the largest overlap
       let mut best = (0.0, 0);
@@ -45,6 +50,14 @@ pub fn divide<'a>(n: usize, bucket: I<'a>) -> Vec<I<'a>> {
     for (bbox,inserts) in boxes {
       if !inserts.is_empty() && inserts.len() <= n {
         res.push((bbox,inserts));
+      } else if !inserts.is_empty() && inserts.len() == q_inserts_len {
+        let all_big = inserts.iter()
+          .all(|(p,_)| coord_span_ge(&p.0, q_span.0) || coord_span_ge(&p.1, q_span.1));
+        if all_big {
+          res.push((bbox,inserts));
+        } else {
+          queue.push_back((bbox,inserts));
+        }
       } else if !inserts.is_empty() {
         queue.push_back((bbox,inserts));
       }
@@ -70,5 +83,12 @@ fn overlap_area(bbox: &B, p: &P) -> f32 {
       x * y
     },
     _ => 0.0
+  }
+}
+
+fn coord_span_ge(p: &eyros::Coord<f32>, span: f32) -> bool {
+  match p {
+    eyros::Coord::Scalar(_x) => false,
+    eyros::Coord::Interval(min,max) => (max - min) > span,
   }
 }
